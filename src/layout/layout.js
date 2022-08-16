1ae4c4102753a750211efb5c4b60395b41e91e12
Holllywaterr <holywater@handong.ac.kr>
2022-08-16 17:05:13 +0900

    commit

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,10 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Header from "./header";
 import MapArea from "./MapArea";
 import PlaceList from "./place_list";
 import Review from "./review";
-import SearchList from "./search_list";
 
 const Container = styled.div`
 padding:0;
@@ -17,6 +16,33 @@ display:flex;
 flex-direction:row;
 `;
 
+function Meomeok() {
+    const [data, setData] = useState(null)
+    const [isLoading, setLoading] = useState(false)
+    const [name, setName] = useState(null); 
+    const [id, setId] = useState(null); 
+  
+    useEffect(() => {
+        // setData({name: "111", bio: 222})
+      setLoading(true)
+      fetch('http://127.0.0.1:8000/meomeok/restaurants')
+    .then(type=>type.json())
+    .then(result=>{
+      console.log(result[1].restaurant_name)
+      //console.log(keyword)
+      setName(result.restaurant_name)
+      setId(result.id)
+    });
+  },[]);
+  
+    return (
+      <div>
+        <h1>{name}</h1>
+        <p>{id}</p>
+      </div>
+    )
+  }
+
 const Layout = () => {
     const Tabs={1:<PlaceList />,2:<Review />}
     const [activeTab,setActiveTab]=useState(1);
@@ -26,10 +52,11 @@ const Layout = () => {
         <Container>
             <Header setActiveTab={setActiveTab} setKeyword={setKeyword}/>
             <Body>
-                <MapArea keyword={keyword}/>
-                {activeTab===3 ?<SearchList keyword={keyword}/>:Tabs[activeTab]}
+                 <MapArea keyword={keyword}/>
+                 {Tabs[activeTab]}                
             </Body>
-        </Container>
+            <Meomeok></Meomeok>
+         </Container>
     )
 }
 export default Layout
